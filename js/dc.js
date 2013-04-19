fec218250d097476f7d7985368143bf24b3fdd8d
Mathaeus Dejori <dejori@gmail.com>
2013-04-18 17:09:21 -0700

    basic js

--- js/dc.js
+++ js/dc.js
@@ -12,7 +12,7 @@
  *  limitations under the License.
  */
 dc = {
-    version: "1.2.0",
+    version: "1.3.0",
     constants : {
         CHART_CLASS: "dc-chart",
         DEBUG_GROUP_CLASS: "debug",
@@ -533,12 +533,14 @@ dc.baseChart = function (_chart) {
     _chart.dimension = function (d) {
         if (!arguments.length) return _dimension;
         _dimension = d;
+        _chart.expireCache();
         return _chart;
     };
 
     _chart.group = function (g) {
         if (!arguments.length) return _group;
         _group = g;
+        _chart.expireCache();
         return _chart;
     };
 
@@ -747,17 +749,38 @@ dc.baseChart = function (_chart) {
         return _chart;
     };
 
+    _chart.expireCache = function(){
+         // do nothing in base, should be overridden by sub-function
+        return _chart;
+    };
+
     return _chart;
 };
-dc.coordinateGridChart = function (_chart) {
+dc.marginable = function (_chart) {
+    var _margin = {top: 10, right: 50, bottom: 30, left: 30};
+
+    _chart.margins = function (m) {
+        if (!arguments.length) return _margin;
+        _margin = m;
+        return _chart;
+    };
+
+    _chart.effectiveWidth = function () {
+        return _chart.width() - _chart.margins().left - _chart.margins().right;
+    };
+
+    _chart.effectiveHeight = function () {
+        return _chart.height() - _chart.margins().top - _chart.margins().bottom;
+    };
+
+    return _chart;
+};dc.coordinateGridChart = function (_chart) {
     var DEFAULT_Y_AXIS_TICKS = 5;
     var GRID_LINE_CLASS = "grid-line";
     var HORIZONTAL_CLASS = "horizontal";
     var VERTICAL_CLASS = "vertical";
 
-    _chart = dc.baseChart(_chart);
-
-    var _margin = {top: 10, right: 50, bottom: 30, left: 30};
+    _chart = dc.marginable(dc.baseChart(_chart));
 
     var _parent;
     var _g;
@@ -809,12 +832,6 @@ dc.coordinateGridChart = function (_chart) {
     _chart.chartBodyG = function (_) {
         if (!arguments.length) return _chartBodyG;
         _chartBodyG = _;
-        return _chart;
-    };
-
-    _chart.margins = function (m) {
-        if (!arguments.length) return _margin;
-        _margin = m;
         return _chart;
     };
 
@@ -961,7 +978,7 @@ dc.coordinateGridChart = function (_chart) {
     };
 
     _chart.xAxisLength = function () {
-        return _chart.width() - _chart.margins().left - _chart.margins().right;
+        return _chart.effectiveWidth();
     };
 
     function prepareYAxis(g) {
@@ -1103,7 +1120,7 @@ dc.coordinateGridChart = function (_chart) {
     };
 
     _chart.yAxisHeight = function () {
-        return _chart.height() - _chart.margins().top - _chart.margins().bottom;
+        return _chart.effectiveHeight();
     };
 
     _chart.round = function (_) {
@@ -1490,16 +1507,17 @@ dc.stackableChart = function (_chart) {
         _groupStack.setDefaultAccessor(_chart.valueAccessor());
         _groupStack.addGroup(group, retriever);
 
-        expireCache();
+        _chart.expireCache();
 
         return _chart;
     };
 
-    function expireCache() {
+    _chart.expireCache = function(){
         _allGroups = null;
         _allValueAccessors = null;
         _allKeyAccessors = null;
-    }
+        return _chart;
+    };
 
     _chart.allGroups = function () {
         if (_allGroups == null) {
@@ -1684,13 +1702,13 @@ dc.stackableChart = function (_chart) {
 
     dc.override(_chart, "valueAccessor", function (_) {
         if (!arguments.length) return _chart._valueAccessor();
-        expireCache();
+        _chart.expireCache();
         return _chart._valueAccessor(_);
     });
 
     dc.override(_chart, "keyAccessor", function (_) {
         if (!arguments.length) return _chart._keyAccessor();
-        expireCache();
+        _chart.expireCache();
         return _chart._keyAccessor(_);
     });
 
@@ -3245,4 +3263,188 @@ dc.bubbleOverlay = function(root, chartGroup) {
     _chart.anchor(root, chartGroup);
 
     return _chart;
+};dc.rowChart = function(parent, chartGroup) {
+
+    var _g;
+
+    var _labelOffsetX = 10;
+
+    var _labelOffsetY = 15;
+
+    var _gap = 5;
+
+    var _rowCssClass = "row";
+
+    var _chart = dc.marginable(dc.singleSelectionChart(dc.colorChart(dc.baseChart({}))));
+
+    var _xScale;
+
+    var _xAxis = d3.svg.axis().orient("bottom");
+
+    _chart.doRender = function() {
+        _xScale = d3.scale.linear().domain([0, d3.max(_chart.group().all(), _chart.valueAccessor())]).range([0, _chart.effectiveWidth()]);
+
+        _chart.resetSvg();
+
+        _g = _chart.svg()
+            .append("g")
+            .attr("transform", "translate(" + _chart.margins().left + "," + _chart.margins().top + ")");
+
+        _xAxis.scale(_xScale);
+
+        _g.append("g").attr("class", "axis")
+                        .attr("transform", "translate(0, " + _chart.effectiveHeight() + ")")
+                        .call(_xAxis);
+
+        drawGridLines();
+        drawChart();
+
+        return _chart;
+    };
+
+    _chart.title(function (d) {
+        return _chart.keyAccessor()(d) + ": " + _chart.valueAccessor()(d) ;
+    });
+
+    _chart.label(function (d) {
+        return _chart.keyAccessor()(d);
+    });
+
+    function drawGridLines() {
+        var ticks = _xAxis.tickValues() ? _xAxis.tickValues() : _xScale.ticks(_xAxis.ticks()[0]);
+
+        var gridLineG = _g.append("g")
+                          .attr("class", "grid-line vertical");
+
+        var lines = gridLineG.selectAll("line")
+                             .data(ticks);
+
+        var linesGEnter = lines.enter()
+                               .append("line")
+                               .attr("x1", function (d) { return _xScale(d); })
+                               .attr("y1", function (d) { return 0; })
+                               .attr("x2", function (d) { return _xScale(d); })
+                               .attr("y2", function (d) { return _chart.effectiveHeight(); });
+    }
+
+    function drawChart() {
+        var rows = _g.selectAll("g." + _rowCssClass)
+                     .data(_chart.group().all());
+
+        createElements(rows, _chart.group().all());
+        removeElements(rows);
+        updateElements(rows);
+    }
+
+    function createElements(rows, rowData) {
+        var rowEnter = rows.enter()
+                           .append("g")
+                           .attr("class", function(d, i) {
+                                return _rowCssClass + " _" + i;
+                           });
+
+        rowEnter.append("rect").attr("width", 0);
+
+        createTitles(rowEnter);
+
+        createLabels(rowEnter);
+        updateLabels(rows);
+    }
+
+    function removeElements(rows) {
+        rows.exit().remove();
+    }
+
+    function updateElements(rows) {
+        var n = _chart.group().all().length;
+
+        var height = (_chart.effectiveHeight() - (n + 1) * _gap) / n;
+
+        var rect = rows.attr("transform", function(d, i) { return "translate(0," + ((i + 1) * _gap + i * height) + ")"; })
+                       .select("rect")
+                           .attr("height", height)
+                           .attr("fill", _chart.getColor)
+                           .on("click", onClick)
+                           .classed("deselected", function (d) { return (_chart.hasFilter()) ? !_chart.isSelectedRow(d) : false; })
+                           .classed("selected", function (d) { return (_chart.hasFilter()) ? _chart.isSelectedRow(d) : false; });
+
+        dc.transition(rect, _chart.transitionDuration())
+               .attr("width", function(d) {
+                    return _xScale(_chart.valueAccessor()(d));
+               });
+    }
+
+    function createTitles(rowEnter) {
+        if (_chart.renderTitle()) {
+            rowEnter.append("title").text(function(d) {
+                return _chart.title()(d);
+            });
+        }
+    }
+
+    function createLabels(rowEnter) {
+        if (_chart.renderLabel()) {
+            rowEnter.append("text");
+        }
+    }
+
+    function updateLabels(rows) {
+        if (_chart.renderLabel()) {
+            rows.select("text")
+                        .attr("x", _labelOffsetX)
+                        .attr("y", _labelOffsetY)
+                        .attr("class", function (d, i) {
+                            return _rowCssClass + " _" + i;
+                        })
+                        .text(function(d) {
+                            return _chart.label()(d);
+                        });
+        }
+    }
+
+    function numberOfRows() {
+        return _chart.group().all().length;
+    }
+
+    function rowHeight() {
+        var n = numberOfRows();
+        return (_chart.effectiveHeight() - (n + 1) * _gap) / n;
+    }
+
+    function onClick(d) {
+        _chart.onClick(d);
+    }
+
+    _chart.doRedraw = function() {
+        drawChart();
+        return _chart;
+    };
+
+    _chart.xAxis = function () {
+        return _xAxis;
+    };
+
+    _chart.gap = function(g) {
+        if (!arguments.length) return _gap;
+        _gap = g;
+        return _chart;
+    };
+
+    _chart.labelOffsetX = function (o) {
+        if (!arguments.length) return _labelOffsetX;
+        _labelOffset = o;
+        return _chart;
+    };
+
+    _chart.labelOffsetY = function (o) {
+        if (!aruguments.length) return _labelOffsetY;
+        _labelOffset = o;
+        return _chart;
+    };
+
+    _chart.isSelectedRow = function (d) {
+        return _chart.filter() == _chart.keyAccessor()(d);
+    };
+
+    return _chart.anchor(parent, chartGroup);
 };
